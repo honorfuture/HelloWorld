ec6f9730b4272b4dd049d1ed8fb1a22275f41d1d
Wjj <2511451842@qq.com.com>
2025-05-29 10:42:05 +0800

    创建了main.cpp文件，写了初始的代码

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	
+	vector<int>arr{ 12,24,78 };
+
+	sort(arr.begin(),arr.end());
+
+	return 0;
+
+}
